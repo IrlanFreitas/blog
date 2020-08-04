9566c47f0c172ea48f8b7c34b95497d79324b98f
Irlan <irlan.freitas@hotmail.com>
2020-08-03 23:15:11 -0300

    Ajustes com o Google Analitycs

--- gatsby-config.js
+++ gatsby-config.js
@@ -89,6 +89,12 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-174458690-1",
+      },
+    },
     `gatsby-plugin-sitemap`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
