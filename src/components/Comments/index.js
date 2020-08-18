cc90a2a7be2e29a99c7108c3a490e8ff833f2acc
Irlan <irlan.freitas@hotmail.com>
2020-08-18 20:30:58 -0300

    Ajustes para subir essa versão

--- src/components/Comments/index.js
+++ src/components/Comments/index.js
@@ -1,29 +1,27 @@
 import React from "react"
-import propTypes from 'prop-types'
+import propTypes from "prop-types"
 import ReactDisqusComments from "react-disqus-comments"
 import * as S from "./styled"
 
-const Comments = ({url, title}) => {
-
-    const completeURL = `https://irlanfreitas.me${url}`
+const Comments = ({ url, title }) => {
+  const completeURL = `https://irlanfreitas.me${url}`
 
   return (
     <S.CommentsWrapper>
-    <S.CommentsTitle>Comentários</S.CommentsTitle>
+      <S.CommentsTitle>Comentários</S.CommentsTitle>
       <ReactDisqusComments
         shortname="irlanfreitas"
         identifier={completeURL}
         title={title}
         url={completeURL}
-
       />
     </S.CommentsWrapper>
   )
 }
 
 Comments.propTypes = {
-    url: propTypes.string.isRequired,
-    title: propTypes.string.isRequired
+  url: propTypes.string.isRequired,
+  title: propTypes.string.isRequired,
 }
 
 export default Comments
